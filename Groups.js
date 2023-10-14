a09742af9bb43eec7b19f7a0ca1dbda3fa072189
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2023-10-14 18:54:31 +0300

    update: add delete grou button, num of tasks in the group & styling update

--- Groups.js
+++ Groups.js
@@ -1,19 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { getGroups } from "./firebase";
+import { useRoute } from "@react-navigation/native";
+import { getGroups, deleteGroup, getNumTasks } from "./firebase";
 
 const GroupListScreen = () => {
+  const route = useRoute();
   const navigation = useNavigation();
   const [groups, setGroups] = useState([]);
+  const newGroup = route.params?.newGroup;
 
   useEffect(() => {
     async function fetchGroups() {
       const groupsData = await getGroups();
-      setGroups(groupsData);
+      const updatedGroups = await Promise.all(
+        groupsData.map(async (group) => {
+          const numTasks = await getNumTasks(group);
+          group.numTasks = numTasks;
+          return group;
+        })
+      );
+      setGroups(updatedGroups);
     }
     fetchGroups();
-  }, []);
+  }, [newGroup]);
 
   const handleGroupPress = (group) => {
     navigation.navigate("TaskList", { group });
@@ -23,16 +39,31 @@ const GroupListScreen = () => {
     navigation.navigate("CreateGroup");
   };
 
+  const handleDeleteGroup = async (group) => {
+    await deleteGroup(group);
+    const updatedGroups = await getGroups();
+    setGroups(updatedGroups);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Group List</Text>
+      <Text style={styles.title}>Groups</Text>
       <FlatList
         data={groups}
         keyExtractor={(group) => group.id}
         renderItem={({ item }) => (
-          <TouchableOpacity style={styles.groupItem} onPress={() => handleGroupPress(item)}>
-            <Text style={styles.groupName}>{item.name}</Text>
-          </TouchableOpacity>
+          <View style={styles.groupItem}>
+            <TouchableOpacity onPress={() => handleGroupPress(item)}>
+              <Text style={styles.groupName}>{item.name}</Text>
+              <Text style={styles.numTasks}>{item.numTasks} Tasks</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.deleteButton}
+              onPress={() => handleDeleteGroup(item)}
+            >
+              <Text style={styles.buttonText}>Delete</Text>
+            </TouchableOpacity>
+          </View>
         )}
       />
       <TouchableOpacity style={styles.newGroupButton} onPress={handleNewGroup}>
@@ -55,19 +86,34 @@ const styles = StyleSheet.create({
     color: "black",
   },
   groupItem: {
-    backgroundColor: "lightgray",
-    padding: 12,
+    backgroundColor: "#f2f3f5",
+    padding: 16,
     marginBottom: 12,
     borderRadius: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   groupName: {
     fontSize: 16,
     color: "black",
+    fontWeight: "bold",
+  },
+  numTasks: {
+    fontSize: 14,
+    color: "gray",
+  },
+  deleteButton: {
+    backgroundColor: "red",
+    padding: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
   },
   newGroupButton: {
     backgroundColor: "black",
     padding: 12,
     alignItems: "center",
+    justifyContent: "center",
     borderRadius: 8,
   },
   buttonText: {
