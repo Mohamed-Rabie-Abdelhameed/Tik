d310e8c2b1899375c1c8ee924484feb3658c82b1
M_Rabie <mohamed.rabie.abdelhameed@gmail.com>
2023-10-14 19:04:37 +0300

    finish app

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { SafeAreaView } from "react-native";
+
+import LoginScreen from "./Login.js";
+import GroupListScreen from "./Groups.js";
+import TaskListScreen from "./Tasks.js";
+import CreateGroupScreen from "./CreateGroup.js";
+import CreateTaskScreen from "./CreateTask.js";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, marginTop: 16 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="GroupList"
+            component={GroupListScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="TaskList"
+            component={TaskListScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="CreateGroup"
+            component={CreateGroupScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="CreateTask"
+            component={CreateTaskScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
